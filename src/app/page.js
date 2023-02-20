b60bf2bc1f45e6a2d5c950fb68e5c5a664ce0b6e
obaidahmadzai <obaidullahkhan206@gmail.com>
2023-02-20 23:28:05 +0430

    docs: nextjs and tailwindcss install into project

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Head from "./head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head title="Obaidullah | Fronted Web Developer" />
+    </div>
+  );
+}
